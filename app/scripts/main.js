c52da913306876c43daba996bc8dcad0044dd724
Eugene Mutai <eugenemutai@gmail.com>
2016-04-12 20:21:01 +0300

    Testing how the blocks render by using template jade file; including all header blocks in it

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-  console.log("Hello world!");
-  // console.log('Uncomment to test if reloading work');
+// Create the event.
+var event = document.createEvent('Event');
+// Define that the event name is 'jqueryLoaded'.
+event.initEvent('jqueryLoaded', true, true);
+
+// target can be any Element or other EventTarget.
+$(document).ready(function($) {
+  Array.prototype.forEach.call(document.querySelectorAll('header[id*="header"]'), function(elem) {
+    elem.dispatchEvent(event);
+  });
 });
+
